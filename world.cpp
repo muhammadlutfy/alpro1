0d554540ca2166f53cfafd6e25962206b3eaf3d3
muhammadlutfy <muhammadlutfy97@gmail.com>
2017-03-13 22:38:18 +0700

    Add files via upload

---
+++ world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>		//Library
+#include <conio.h>
+using namespace std;
+	 
+	int main () { //Fungsi
+	
+	//Definisi Fungsi
+		cout << "HELLO WORLD" <<endl;
+		cout << " I Will Save This World, Aamiin ^^";
+		return 0;
+		
+	getch();	
+	}
